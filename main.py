5d5c1c708f60f8bc1c5599d3d00abe4839ce7f4a
jojo259 <95320642+jojo259@users.noreply.github.com>
2022-01-04 13:05:24 +0000

    done

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
 import requests
-
-apiKey = None
-
-def readApiKey():
-	try:
-		apiKey = open('apiKey.txt').read()
-	except:
-		print('cant read api key, exiting')
-		exit()
-
-def testApiKey():
-	apiUrl = f'https://api.hypixel.net/friends?key={apiKey}&uuid=1f2e58ced9164d55bd3fa7f4a81dd09f'
-	print(apiUrl)
-
-readApiKey()
-print('testing api key')
-testApiKey()
+import time
 
 def getApi(url):
-	#print(url)
 	try:
 		receivedApi = requests.get(url,timeout=1)
 		receivedApi = receivedApi.json()
@@ -27,5 +10,138 @@ def getApi(url):
 	except:
 		return None
 
-while notFound:
-	apiUrl = f'https://api.hypixel.net/friends?key={apiKey}&uuid={toCheckUuid}'
+def readApiKey():
+	try:
+		global apiKey
+		apiKey = open('apiKey.txt').read()
+	except:
+		print('failed to read api key, exiting')
+		exit()
+
+def testApiKey():
+	global apiKey
+	apiUrl = f'https://api.hypixel.net/friends?key={apiKey}&uuid=1f2e58ced9164d55bd3fa7f4a81dd09f'
+	dataReceived = getApi(apiUrl)
+	if dataReceived != None:
+		print('api key works')
+	else:
+		print('invalid api key, exiting')
+		exit()
+
+def readPathfindTargets():
+	try:
+		global pathfindFrom
+		pathfindFrom = open('pathfindFrom.txt').read()
+		print(f'got pathfindFrom:	{pathfindFrom}')
+	except:
+		print('failed to read pathfindFrom, exiting')
+		exit()
+	try:
+		global pathfindTo
+		pathfindTo = open('pathfindTo.txt').read()
+		print(f'got pathfindTo:		{pathfindTo}')
+	except:
+		print('failed to read pathfindTo, exiting')
+		exit()
+
+def getUuid(username):
+	try:
+		apiUrl = f'https://api.mojang.com/users/profiles/minecraft/{username}'
+		dataReceived = getApi(apiUrl)
+		uuid = dataReceived['id']
+		return uuid
+	except:
+		pass
+
+def getFriends(getUuid):
+	gotFriends = False
+	apiUrl = f'https://api.hypixel.net/friends?key={apiKey}&uuid={getUuid}'
+	while True:
+		dataReceived = getApi(apiUrl)
+		friendsList = []
+		if dataReceived != None:
+			if 'records' not in dataReceived:
+				print('rate limited')
+				continue
+			for friendsEntry in dataReceived['records']:
+				uuidSender = friendsEntry['uuidSender']
+				uuidReceiver = friendsEntry['uuidReceiver']
+
+				diffUuid = None
+				if getUuid == uuidSender:
+					friendsList.append(uuidReceiver)
+				else:
+					friendsList.append(uuidSender)
+			return friendsList
+	return []
+
+def getUsernameFromUuid(uuid):
+	returned = False
+	while not returned:
+		try:
+			apiUrl = f'https://api.mojang.com/user/profiles/{uuid}/names'
+			dataReceived = getApi(apiUrl)
+			dataReceivedLen = len(dataReceived)
+			lastUsername = dataReceived[dataReceivedLen - 1]['name']
+			returned = True
+			return lastUsername
+		except:
+			return 'nil'
+
+def uuidListToUsernames(uuidList):
+	for i, uuid in enumerate(uuidList):
+		uuidList[i] = getUsernameFromUuid(uuid)
+	return uuidList
+
+def pathfindFriends(pfFrom, pfTo):
+	checkQueue = [pfFrom]
+	alrCheckedOrQueued = {}
+	nodePrev = {}
+	while len(checkQueue) > 0:
+		curCheck = checkQueue[0]
+		curFriends = getFriends(curCheck)
+		alrCheckedOrQueued[curCheck] = True
+		checkQueue.pop(0)
+		print(f'checking {curCheck}, checked {len(alrCheckedOrQueued) - len(checkQueue)}, to potentially check {len(checkQueue)}')
+		for friend in curFriends:
+			if friend == pfTo:
+				print('found path')
+				nodePrev[pfTo] = curCheck
+				break
+			if friend not in alrCheckedOrQueued:
+				checkQueue.append(friend)
+				alrCheckedOrQueued[friend] = True
+				nodePrev[friend] = curCheck
+		else:
+			continue
+		break
+	else:
+		print('not found')
+		return [] #no path found
+	path = [pfTo]
+	while path[len(path) - 1] != pfFrom:
+		path.append(nodePrev[path[len(path) - 1]])
+	return list(reversed(path))
+
+global apiKey
+readApiKey()
+print('testing api key')
+testApiKey()
+
+global pathfindFrom
+global pathfindTo
+print('reading pathfinding targets')
+readPathfindTargets()
+
+pathfindFrom = getUuid(pathfindFrom)
+pathfindTo = getUuid(pathfindTo)
+
+timerStart = time.time()
+pathFound = pathfindFriends(pathfindFrom, pathfindTo)
+timerDone = time.time()
+print(f'time: {int((timerDone - timerStart)*1000)}ms')
+pathFoundUsernames = uuidListToUsernames(pathFound)
+pathFoundUsernamesPretty = ' --> '.join(pathFoundUsernames)
+
+print()
+print(pathFoundUsernamesPretty)
